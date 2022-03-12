6d25e9c55d1882c716ba3fd9af9a1e6d9ac828ff
Shafin580 <shafinwork580@gmail.com>
2022-03-12 23:20:05 +0600

    heroku-deployment-01

--- JanataWifiFullStackTest/settings.py
+++ JanataWifiFullStackTest/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
+import django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +29,7 @@ SECRET_KEY = 'django-insecure-^&9+apdw#&=a4@zxd=ssah0y@5k%lxu(dhys-(l_u+qha0*^5(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['janatawififullstacktest.herokuapp.com']
 
 
 # Application definition
@@ -126,6 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.setting(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn JanataWifiFullStackTest.wsgi --log-file=-
